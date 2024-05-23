2f3da401d82c3d678d21a7b9d1972c322f26c598
Gabriel Castro <c4str0@live.com>
2024-05-23 00:48:05 -0300

    feat: Initial Go project commit

---
+++ app.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, World! My name is %s and I am %d years old.\n", "John", 30)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zC4sTr0/GunBoundTH-Server
+
+go 1.22.3
