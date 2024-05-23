ce6b2986877e7be6b06042fff5633d2e20b424a6
Gabriel Castro <c4str0@live.com>
2024-05-23 01:41:06 -0300

    feat: Added cryptography package to main

--- app.go
+++ app.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/zC4sTr0/GunBoundTH-Server/cryptography"
+)
 
 func main() {
-	fmt.Printf("Hello, World! My name is %s and I am %d years old.\n", "John", 30)
+	data, err := hex.DecodeString("E4AE6422B374D8779DC2F3695810650F")
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
+		return
+	}
+
+	decrypted, err := cryptography.GunboundStaticDecrypt(data, 1)
+	if err != nil {
+		fmt.Println("Error decrypting data:", err)
+		return
+	}
+
+	fmt.Println(cryptography.StringDecode(decrypted))
 }
